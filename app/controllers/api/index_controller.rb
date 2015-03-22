9a2ce80dffccf93129a0d00846a9bb4ced43654d
robj <robert.jennings@gmail.com>
2015-03-23 10:47:03 +1100

    filter moved into model as class method, use map instead of loops

--- app/controllers/api/index_controller.rb
+++ app/controllers/api/index_controller.rb
@@ -1,9 +1,24 @@
 class Api::IndexController < ApplicationController
 
 
+    protect_from_forgery :except => :create 
+
 
     def index
-        render json: [1]
+        render text: 'streamco coding challenge'
+    end
+
+
+    def create
+
+        shows = params[:payload].map {|show_params| Show.new(show_params)}
+
+        filteredShows =  Show.has_episodes_and_drm(shows)
+        filteredShowsJSON = filteredShows.map { |show| show.to_json }
+
+        render json: {response: filteredShowsJSON }
+
+
     end
 
 
