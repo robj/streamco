e69fdf230351c8f0acdce6f1fdbc6db7540f3494
robj <robert.jennings@gmail.com>
2015-03-23 10:52:17 +1100

    consistent snake_case

--- app/controllers/api/index_controller.rb
+++ app/controllers/api/index_controller.rb
@@ -14,7 +14,7 @@ class Api::IndexController < ApplicationController
         shows = params[:payload].map {|show_params| Show.new(show_params)}
 
         filtered_shows =  Show.has_episodes_and_drm(shows)
-        filtered_shows_json = filteredShows.map { |show| show.to_json }
+        filtered_shows_json = filtered_shows.map { |show| show.to_json }
 
         render json: { response: filtered_shows_json }
 
