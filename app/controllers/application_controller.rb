ccef2f0e65c3bca94075c5bec0290f23a522ae22
robj <robert.jennings@gmail.com>
2015-03-23 12:36:44 +1100

    json parse error handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+
+  rescue_from 'JSON::ParserError' do |exception|
+           error_message = {error: "Could not decode request: JSON parsing failed"}
+           render json: error_message, status: 400
+  end
+
+
 end
