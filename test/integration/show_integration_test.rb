15e3582dae2995f952e73e3bfc00a2654d024820
robj <robert.jennings@gmail.com>
2015-03-23 11:33:28 +1100

    show integration test

--- test/integration/show_integration_test.rb
+++ test/integration/show_integration_test.rb
@@ -7,7 +7,23 @@ end
 
 describe "Show integration" do
 
-  it "does nothing" do
+  it "expected response when POSTed example JSON payload" do 
+
+        filename = Rails.root.to_s +  '/test/example_payload.json'
+        file = File.read(filename)
+        post_params = JSON.parse(file)
+
+
+        post '/', post_params
+
+        assert last_response.successful?
+        assert last_response_json['response'].count.must_equal 7
+
+        assert last_response_json['response'].first.has_key?('image')
+        assert last_response_json['response'].first.has_key?('slug')
+        assert last_response_json['response'].first.has_key?('title')
+
+
   end
 
 end
