f6ce6b1d8e709f1820594425d9f2ad1ecdc69ca3
robj <robert.jennings@gmail.com>
2015-03-23 12:39:22 +1100

    json validation now in application controller rescue handler

---
+++ app/controllers/shows_filter_controller.rb
@@ -0,0 +1,27 @@
+class ShowFilterController < ApplicationController
+
+
+    protect_from_forgery :except => :create 
+
+
+    def index
+        render text: 'streamco coding challenge'
+    end
+
+
+    def create
+
+        shows = params[:payload].map {|show_params| Show.new(show_params)}
+
+        filtered_shows =  Show.has_episodes_and_drm(shows)
+        filtered_shows_json = filtered_shows.map { |show| show.to_json }
+
+        render json: { response: filtered_shows_json }
+
+
+    end
+
+
+
+
+end
